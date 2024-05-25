de01ed7de47378c0e04027e341f7c6ed8a7dd126
Alfred Valderrama <alfred98valderrama@gmail.com>
2024-05-25 19:28:38 +0800

    feat(*.tf)!: added terraform module

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,54 +6,69 @@
  * ***Author***: Alfred Valderrama (@redopsbay) <br>
 */
 
-provider "http" {}
 data "aws_caller_identity" "current" {}
-data "http" "lambda_zip" {
-  url = "TODO"
+
+
+/* Download the release file */
+resource "null_resource" "lambda_file" {
+  provisioner "local-exec" {
+    command = "curl -Lo lambda.zip ${var.release_url}"
+  }
 }
 
-resource "local_file" "lambda_file" {
-  content  = data.http.lambda_zip.body
-  filename = var.lambda_local_path
-}
 
-module "lambda_cloudcycle" {
-  source        = "terraform-aws-modules/lambda/aws"
-  version       = "7.4.0"
+/* Lambda Function */
+
+resource "aws_lambda_function" "cloudcycle" {
+  filename      = var.lambda_local_path
   function_name = var.lambda_name
-  description   = "CloudCycle for cost savings"
+  role          = var.lambda_iam_role_arn
   handler       = "bootstrap"
-  runtime       = "provided.al2023"
+  description   = "Lambda CloudCycle for Cost Savings"
   publish       = true
-  source_path   = var.lambda_local_path
-  //role          = ""
 
-  tags = merge(var.tags, {
-    Name        = "CloudCycle"
-    Description = "CloudCycle for cost savings"
-  })
+
+  runtime = "provided.al2023"
+
+  tags = var.tags != null ? merge(var.tags, {
+    Function        = "CloudCycle for Cost Savings"
+    }) : { Function = "CloudCycle for Cost Savings"
+  }
+  depends_on = [null_resource.lambda_file]
 }
 
+/* Event Bridge Rule that run's every 15 minute window */
 
-module "eventbridge" {
-  source     = "terraform-aws-modules/eventbridge/aws"
-  version    = "3.3.1"
-  create_bus = false
+resource "aws_cloudwatch_event_rule" "cloudcycle_schedule" {
+  name        = "CloudCycle-Event-Rule"
+  description = "CloudCycle Lambda gets trigger every 15 minutes."
 
-  rules = {
-    crons = {
-      description         = "Run the event bridge every 15 minutes"
-      schedule_expression = "cron(*/15 * * ? *)"
-    }
+  schedule_expression = "cron(0/15 * * * ? *)"
+  depends_on          = [aws_lambda_function.cloudcycle]
+  tags = var.tags != null ? merge(var.tags, {
+    Function        = "CloudCycle for Cost Savings"
+    }) : { Function = "CloudCycle for Cost Savings"
   }
+}
 
-  targets = {
-    crons = [
-      {
-        name  = "cloudcycle"
-        arn   = module.lambda_cloudcycle.lambda_function_arn
-        input = jsonencode({ "useless" : "input" })
-      }
-    ]
-  }
+/* Set event bridge rule target */
+
+resource "aws_cloudwatch_event_target" "cloudcycle_lambda_target" {
+  rule      = aws_cloudwatch_event_rule.cloudcycle_schedule.name
+  target_id = var.lambda_name
+  arn       = aws_lambda_function.cloudcycle.arn
+  depends_on = [
+    aws_lambda_function.cloudcycle,
+    aws_cloudwatch_event_rule.cloudcycle_schedule
+  ]
+}
+
+/* Allow event bridge to trigger lambda function */
+
+resource "aws_lambda_permission" "allow_event_bridge" {
+  statement_id  = "AllowExecutionFromEventBridge"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.cloudcycle.function_name
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.cloudcycle_schedule.arn
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -21,10 +21,22 @@ variable "region" {
 variable "tags" {
   type        = map(any)
   description = "Resource tag"
+  default     = null
 }
 
 variable "lambda_local_path" {
   type        = string
   description = "Specifies the `lambda.zip` local path"
-  default     = "/tmp/lambda.zip"
+  default     = "lambda.zip"
+}
+
+variable "lambda_iam_role_arn" {
+  type        = string
+  description = "Specify arn of lambda"
+}
+
+variable "release_url" {
+  type        = string
+  description = "Release url from github [https://github.com/redopsbay/cloudcycle](https://github.com/redopsbay/cloudcycle) `release.zip`"
+  default     = "https://github.com/redopsbay/cloudcycle/releases/download/v1.0.0-alpha/release.zip"
 }
